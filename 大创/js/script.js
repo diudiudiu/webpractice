aa661b3f5cfc75658791f772276fab4ea276327e
diudiudiu <171738749@qq.com>
2018-11-12 23:25:35 +0800

    update11.12

--- 大创/js/script.js
+++ 大创/js/script.js
@@ -1,6 +1,26 @@
 window.onload=function() {
 	var reference=document.getElementById('reference');
+	var system=document.getElementById('system');
+	var database=document.getElementById('database');
+	var chart=document.getElementById('chart');
+	var download=document.getElementById('download');
+	var algorithm=document.getElementById('algorithm');
 	reference.onclick=function(){
 		window.location.href="Reference/index.html"; 
+	}
+	system.onclick=function(){
+		window.location.href="System/index.html"; 
+	}
+	database.onclick=function(){
+		window.location.href="Database/index.html"; 
+	}
+	download.onclick=function(){
+		window.location.href="Download/index.html"; 
+	}
+	chart.onclick=function(){
+		window.location.href="Chart/index.html"; 
+	}
+	algorithm.onclick=function(){
+		window.location.href="Algorithm/index.html"; 
 	}
 }
