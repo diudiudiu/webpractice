a997ab83b18824743c47783e07db939110df867e
diudiudiu <171738749@qq.com>
2018-11-27 23:13:11 +0800

    update11.27

--- 大创/Database/js/script.js
+++ 大创/Database/js/script.js
@@ -26,56 +26,38 @@ window.onload=function(){
         {'src':'../../images/yundong/yundong(57).jpg'},
         {'src':'../../images/yundong/yundong(58).jpg'},
         {'src':'../../images/yundong/yundong(59).jpg'},
-        // {'src':'../../images/yundong/yundong(60).jpg'},
-        // {'src':'../../images/yundong/yundong(61).jpg'},
-        // {'src':'../../images/yundong/yundong(62).jpg'},
-        // {'src':'../../images/yundong/yundong(63).jpg'},
-        // {'src':'../../images/yundong/yundong(64).jpg'},
-        // {'src':'../../images/yundong/yundong(65).jpg'}
         ]
     };
     var data2={
         'data':[
-        {'src':'../../images/yundong/yundong(56).jpg'},
-        {'src':'../../images/yundong/yundong(57).jpg'},
-        {'src':'../../images/yundong/yundong(58).jpg'},
-        {'src':'../../images/yundong/yundong(59).jpg'},
         {'src':'../../images/yundong/yundong(60).jpg'},
         {'src':'../../images/yundong/yundong(61).jpg'},
         {'src':'../../images/yundong/yundong(62).jpg'},
         {'src':'../../images/yundong/yundong(63).jpg'},
-        {'src':'../../images/yundong/yundong(64).jpg'},
-        {'src':'../../images/yundong/yundong(65).jpg'}]
+        ]
     };
     var data3={
         'data':[
-        {'src':'../../images/yundong/yundong(56).jpg'},
-        {'src':'../../images/yundong/yundong(57).jpg'},
-        {'src':'../../images/yundong/yundong(58).jpg'},
-        {'src':'../../images/yundong/yundong(59).jpg'},
-        {'src':'../../images/yundong/yundong(60).jpg'},
-        {'src':'../../images/yundong/yundong(61).jpg'},
         {'src':'../../images/yundong/yundong(62).jpg'},
         {'src':'../../images/yundong/yundong(63).jpg'},
         {'src':'../../images/yundong/yundong(64).jpg'},
-        {'src':'../../images/yundong/yundong(65).jpg'}]
+        {'src':'../../images/yundong/yundong(65).jpg'}
+        ]
     };
     var more1=document.getElementById('wall1more');
     var more2=document.getElementById('wall2more');
     var more3=document.getElementById('wall3more');
-    
+    more1.onclick=function(){
+        more('wall1',1);
+    }
     more2.onclick=function(){
         more('wall2',2);
     }
     more3.onclick=function(){
         more('wall3',3);
     }
-    for(var i=1;i<=3;i++){
-        waterfall('wall'+i,'out',i);
-    }
-    more1.onclick=function(){
-        more('wall1',1);
-    }
+    
+    
     function more(wall,num){
             var oParent = document.getElementById(wall);// 父级对象
             // oParent.style.height=(oParent.offsetHeight+50)+'px';
@@ -116,44 +98,6 @@ window.onload=function(){
                     oBox.appendChild(oImg);
                 }
             }
-            waterfall(wall,'out');
     };
     
-}
-function waterfall(parent,classname){
-    var wall=document.getElementById(parent);
-    var out=getClassName(wall,classname);
-    var arr1=[];
-    var column=Math.floor(wall.offsetWidth/out[0].offsetWidth);
-    for(var i=0;i<out.length;i++){
-        var temp=out[i].offsetHeight;
-        if(i<column){
-            arr1[i]=temp;
-        }else{
-            var minH=Math.min.apply(null,arr1);
-            var index=getminHIndex(arr1,minH);
-            out[i].style.position='absolute';//设置绝对位移
-            out[i].style.top=minH+'px';
-            out[i].style.left=out[index].offsetLeft+'px';
-            arr1[index]+=out[i].offsetHeight;
-        }
-    }      
-}
-function getminHIndex(arr,minH){
-    for(var i in arr){
-        if(arr[i]==minH){
-            return i;
-        }
-    }
-}
-
-function getClassName(parent,classname){
-	var arr=[];
-	var out=parent.getElementsByTagName('*');
-	for(var i=0;i<out.length;i++){
-		if(out[i].className==classname){
-			arr.push(out[i]);
-		}
-	}
-	return arr;
 }
